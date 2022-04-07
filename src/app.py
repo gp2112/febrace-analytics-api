f35034ae0c432f05af28d611ae15667bb46e2ac8
gp2112 <guircp@usp.br>
2022-04-07 11:44:00 -0300

    remodel

--- src/app.py
+++ src/app.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI, Response
 import athena
+import json
 
 app = FastAPI()
 
@@ -12,58 +13,33 @@ def parse_fields(fields: str) -> list:
     fields = fields.replace('"', '').replace("'", "") #prevent sqlinjection
     return fields.replace(' ', '').split(',')
 
-@app.get('/escolas')
-async def escolas(fields: str, limit: int, response: Response) -> dict:
-    fields = parse_fields(fields)
-    a = athena.Athena(awsCatalog, 'education', queryResultLocation, profile=awsProfile)
-    
-    try:
-        data = a.query(f'SELECT {",".join(fields)} FROM "escolas_parquet" LIMIT {limit}')
-    except Exception as e:
-        response.status_code = 400
-        return {'error':str(e)}
+# gets Athena's right table's name
+def get_table(name: str) -> str:
+    with open('tables.json') as f:
+        tables = json.load(f)
+    if name not in tables:
+        raise Exception("Table doesn't exists.")
+    return tables[name]
 
-    return data
 
-@app.get('/matriculas')
-async def matriculas(fields: str, limit: int, response: Response) -> dict:
+@app.get('/{table}')
+async def query_table(table: str, fields: str, limit: int, response: Response) -> dict:
     fields = parse_fields(fields)
     a = athena.Athena(awsCatalog, 'education', queryResultLocation, profile=awsProfile)
     try:
-        data = a.query(f'SELECT {",".join(fields)} FROM "matriculas-parquet" LIMIT {limit}')
-    except Exception as e:
-        response.status_code = 400
-        return {'error':str(e)}
-    return data
-
-@app.get('/docentes')
-async def docentes(fields: str, limit: int, response: Response) -> dict:
-    fields = parse_fields(fields)
-    a = athena.Athena(awsCatalog, 'education', queryResultLocation, profile=awsProfile)
-    try:
-        data = a.query(f'SELECT {",".join(fields)} FROM "docentes_parquet" LIMIT {limit}')
-    except Exception as e:
-        response.status_code = 400
-        return {'error':str(e)}
-
-    return data
-
-@app.get('/turmas')
-async def turmas(fields: str, limit: int, response: Response) -> dict:
-    fields = parse_fields(fields)
-    a = athena.Athena(awsCatalog, 'education', queryResultLocation, profile=awsProfile)
-    try:
-        data = a.query(f'SELECT {",".join(fields)} FROM "turmas_parquet" LIMIT {limit}')
+        table = get_table(table)
+        data = a.query(f'SELECT {",".join(fields)} FROM "{tables[table]}" LIMIT {limit}')
     except Exception as e:
         response.status_code = 400
         return {'error':str(e)}
     return data
+ 
 
 @app.get('/{table}/cols')
 async def columns(table: str, response: Response) -> dict:
     a = athena.Athena(awsCatalog, 'education', queryResultLocation, profile=awsProfile)
     try:
-        data = a.table_columns(table)
+        data = a.table_columns(tables[table])
     except Exception as e:
         response.status_code = 404
         return {'error':str(e)}
