f6ccce114097c542a8ebf157a5012fba254c3547
gp2112 <guircp@usp.br>
2022-04-08 11:21:16 -0300

    filters

--- src/app.py
+++ src/app.py
@@ -9,9 +9,31 @@ queryResultLocation = 's3://mestrado-educacao/application-athena-query/'
 awsProfile = 'febrace'
 awsCatalog = 'AwsDataCatalog'
 
+
 def parse_fields(fields: str) -> list:
     fields = fields.replace('"', '').replace("'", "") #prevent sqlinjection
     return fields.replace(' ', '').split(',')
+
+def make_query(table: str, fields: list='*', filters: list=None, limit: int=LIMIT) -> str:
+    if fields is not None: fields="', '".join(fields)
+    if filters is None: filters=list() 
+    query = f'SELECT "{fields}" FROM "{table}"'
+    
+    if len(filters) > 0:
+        query += ' WHERE'
+
+    for fil in filters:
+        query += f" {fil['field']}"
+        if 'equals' in fil:
+            query += f"='{fil['equals']}'"
+        elif 'bigger' in fil:
+            query += f">{fil['bigger']}"
+        elif 'lower' in fil:
+            query += f"<{fil['lower']}"
+    query += f" LIMIT {limit}"
+    print(query)
+    return query
+    
 
 # gets Athena's right table's name
 def get_table(name: str) -> str:
@@ -23,23 +45,28 @@ def get_table(name: str) -> str:
 
 
 @app.get('/{table}')
-async def query_table(table: str, fields: str, limit: int, response: Response) -> dict:
+async def query_table(table: str, fields: str, response: Response, 
+                        filters: str=None, limit: int=LIMIT) -> dict:
     fields = parse_fields(fields)
+    table = table.replace('"', '').replace("'", "") # no sqlinjection :)
     a = athena.Athena(awsCatalog, 'education', queryResultLocation, profile=awsProfile)
+    if filters is not None:
+        filters = json.loads(filters)
+       
     try:
         table = get_table(table)
-        data = a.query(f'SELECT {",".join(fields)} FROM "{tables[table]}" LIMIT {limit}')
+        data = a.query(make_query(table, fields, filters, limit))
     except Exception as e:
         response.status_code = 400
         return {'error':str(e)}
-    return data
- 
+    return data 
 
 @app.get('/{table}/cols')
 async def columns(table: str, response: Response) -> dict:
     a = athena.Athena(awsCatalog, 'education', queryResultLocation, profile=awsProfile)
+    table = table.replace('"', '').replace("'", "")
     try:
-        data = a.table_columns(tables[table])
+        data = a.table_columns(get_table(table))
     except Exception as e:
         response.status_code = 404
         return {'error':str(e)}
