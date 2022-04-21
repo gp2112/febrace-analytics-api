0d3c1c5b2fadb3bfbb5678ad1e8b0485fd309b4b
gp2112 <guircp@usp.br>
2022-04-21 16:57:26 -0300

    change app.py to __init__.py

--- febraceapi/__init__.py
+++ febraceapi/__init__.py
@@ -1,19 +1,20 @@
 from fastapi import FastAPI, Response
 import athena
 import json
+from query import make_query
 
 app = FastAPI()
 
-LIMIT = 100 #default limit
+LIMIT = 100  # default limit
 queryResultLocation = 's3://mestrado-educacao/application-athena-query/'
 awsProfile = 'febrace'
 awsCatalog = 'AwsDataCatalog'
 
 
 def parse_fields(fields: str) -> list:
-    fields = fields.replace('"', '').replace("'", "") #prevent sqlinjection
+    fields = fields.replace('"', '').replace("'", "")  # prevent sqlinjection
     return fields.replace(' ', '').split(',')
-    
+
 
 # gets Athena's right table's name
 def get_table(name: str) -> str:
@@ -25,35 +26,41 @@ def get_table(name: str) -> str:
 
 
 @app.get('/{table}')
-async def query_table(table: str, fields: str, response: Response, 
-                        filters: str=None, limit: int=LIMIT) -> dict:
+async def query_table(table: str, fields: str, response: Response,
+                      filters: str = None, limit: int = LIMIT) -> dict:
+
     fields = parse_fields(fields)
-    table = table.replace('"', '').replace("'", "") # no sqlinjection :)
-    a = athena.Athena(awsCatalog, 'education', queryResultLocation, profile=awsProfile)
+    table = table.replace('"', '').replace("'", "")  # no sqlinjection :)
+    a = athena.Athena(awsCatalog, 'education',
+                      queryResultLocation, profile=awsProfile)
     if filters is not None:
         filters = json.loads(filters)
     print(filters)
-       
+
     try:
         table = get_table(table)
         data = a.query(make_query(table, fields, filters, limit))
     except Exception as e:
         response.status_code = 400
-        return {'error':str(e)}
-    return data 
+        return {'error': str(e)}
+    return data
+
 
 @app.get('/{table}/cols')
 async def columns(table: str, response: Response) -> dict:
-    a = athena.Athena(awsCatalog, 'education', queryResultLocation, profile=awsProfile)
+    a = athena.Athena(awsCatalog, 'education',
+                      queryResultLocation, profile=awsProfile)
+
     table = table.replace('"', '').replace("'", "")
     try:
         data = a.table_columns(get_table(table))
     except Exception as e:
         response.status_code = 404
-        return {'error':str(e)}
-    
+        return {'error': str(e)}
+
     return data
+
 
 @app.get('/')
 async def root():
-    return {'main':'root'}
+    return {'main': 'root'}
